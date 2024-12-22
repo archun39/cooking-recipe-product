0415e06131f218a306a60f53cf1827a2a6f1cd01
Seonghyun Kim <archun39@kookmin.ac.kr>
2024-12-22 16:13:43 +0900

    FEAT : gpt-4o-mini를 활용한 요리 스크립트 요약 모델 구현

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import setting
 
 class ModelManager:
-    def __init__(self):
+    def __init__(self, setting=setting):
         load_dotenv()
         self.api_key = os.getenv('OPENAI_API_KEY')
 
@@ -12,7 +12,32 @@ class ModelManager:
             raise ValueError("OPENAI_API_KEY를 설정해 주세요.")
         
         openai.api_key = self.api_key
-        self.max_tokens = 500
+        self.MAX_TOKENS = 500
         self.model_setting = {
-            
+            "model": setting.model,
+            "role_message": {
+                "role": "system",
+                "content": setting.prompt
+            }   
         }
+
+    def get_summary(self, data: str) -> str:
+        try:
+            response = openai.chat.completions.create(
+                model=self.model_setting["model"],
+                messages=[
+                    self.model_setting["role_message"],
+                    {"role": "user",
+                     "content": data
+                    }
+                ],
+                temperature=0,
+                max_tokens=self.MAX_TOKENS
+            )
+            result = response.choices[0].message.content
+            return result
+        
+        except openai.error.OpenAIError as e:
+            print(f"OpenAI 오류: {e}")
+            return "오류 발생"
+        
