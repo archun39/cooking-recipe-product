63315e8d113d511aea84586b7b9b61fde07945f2
Seonghyun Kim <archun39@kookmin.ac.kr>
2024-12-22 18:22:01 +0900

    FEAT : main파일에 판별 및 전처리 기능 추가

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ import input, model
 def main():
     script = input.InputManager().read_file(4) # 1~4
     gpt = model.ModelManager()
-    res = gpt.get_summary(script)
 
-    print(res)
+    if(gpt.is_cooking_script()):
+        data = gpt.preprocessing_data(script)
+        res = gpt.get_summary(data)
+        print(res)
+    else:
+        print("요리 스크립트가 아닙니다.")
+
 
 if __name__ == "__main__":
     main()
