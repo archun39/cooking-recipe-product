4073e77bdbc00b2ee9d7ce6f41597494fc21a800
Seonghyun Kim <archun39@kookmin.ac.kr>
2024-12-22 16:05:11 +0900

    FEAT : ModelManager init 추가

---
+++ src/model.py
@@ -0,0 +1,16 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+class ModelManager:
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv('OPENAI_API_KEY')
+
+        if self.api_key is None:
+            raise ValueError("OPENAI_API_KEY를 설정해 주세요.")
+        
+        openai.api_key = self.api_key
+        self.max_tokens = 500
+        self.model_setting = {
+        }
